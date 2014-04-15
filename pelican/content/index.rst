a4e665ef52e5ac2b547b7af9cfc827c9ce21bfca
Marcelo Tambalo <marcelo.zokis@gmail.com>
2014-04-15 14:32:49 -0300

    inicio com pelican

---
+++ pelican/content/index.rst
@@ -0,0 +1,11 @@
+My super title
+##############
+
+:date: 2010-10-03 10:20
+:tags: thats, awesome
+:category: yeah
+:slug: my-super-post
+:author: Alexis Metaireau
+:summary: Short version for index and feeds
+
+This will be turned into :abbr:`HTML (HyperText Markup Language)`.
---
+++ pelican/pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Marcelo Fonseca Tambalo'
+SITENAME = u'Zokis Blog'
+SITEURL = 'http://zokis.github.io'
+
+TIMEZONE = 'America/Sao_Paulo'
+
+DEFAULT_LANG = u'pt'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+THEME = "/home/znc/projetos/pelican-themes/bootstrap"
+
+# Blogroll
+LINKS = (
+    ('Python.org', 'http://python.org/'),
+    ('Pelican', 'http://getpelican.com/'),
+    ('Django', 'https://www.djangoproject.com/'),
+    ('Python Faster Way', 'http://pythonfasterway.uni.me/'),
+)
+
+# Social widget
+SOCIAL = (
+    ('Github', 'https://github.com/zokis/'),
+    ('Bitbucket', 'https://bitbucket.org/zokis'),
+)
+
+DEFAULT_PAGINATION = 15
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
